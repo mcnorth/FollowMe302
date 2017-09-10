a944fe9213744f9991cc48f49add6db47ca416e5
DESKTOP-LKCNPUO\kelli <kelliemcnaughton@gmail.com>
2017-09-10 17:37:48 +1000

    Added c# to edit compnay details

--- FollowMe302/FollowMe302/EditCompanyDetails.aspx.cs
+++ FollowMe302/FollowMe302/EditCompanyDetails.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FollowMe302.Csharp;
+using System.Data.SqlClient;
 
 namespace FollowMe302
 {
@@ -11,7 +13,96 @@ namespace FollowMe302
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string compUserName = Session["name"].ToString();
+            string compFollowId = Session["fmID"].ToString();
 
+            //adds the session variable
+            lblSession.Text = "Hello " + compUserName;
+        }
+
+        protected void btnEditCoDetails_Click(object sender, EventArgs e)
+        {
+            string compName = Session["name"].ToString();
+            //adds user input from the text boxes to an object
+            MemberEntity member = new MemberEntity();
+            member.CompanyRep = txtEditRepName.Text;
+            member.PhoneNumber = txtEditPhoneNumber.Text;
+            member.Address = txtEditAddress.Text;
+            member.Suburb = txtEditSuburb.Text;
+            member.Postcode = txtEditPostcode.Text;
+
+            //creates connection and queries
+            SqlConnection con = new SqlConnection(@"Data Source=182.50.133.109; Database=FollowMe; Integrated Security=False; User ID=kellie; Password=rQp45a1^; Connect Timeout=15; Encrypt=False; Packet Size=4096");
+
+            //SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|FollowMeDatabase.mdf; Integrated Security=True");
+
+            SqlDataReader rdr = null;
+
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [_CompanyDetails] WHERE [companyRep] = '" + member.CompanyRep + "'", con);
+
+            SqlCommand modify = new SqlCommand("UPDATE _CompanyDetails SET companyRep=@companyRep, phoneNumber=@phoneNumber, address=@address, suburb=@suburb, postcode=@postcode", con);
+
+            SqlCommand insert = new SqlCommand(" INSERT INTO _CompanyDetails (companyRep, phoneNumber, address, suburb, postcode)" +
+                                    "VALUES (@companyRep, @phoneNumber, @address, @suburb, @postcode)", con);
+
+
+            SqlCommand insertId = new SqlCommand("UPDATE _CompanyDetails SET companyId = (SELECT companyId FROM _Company WHERE companyName = '" + compName + "') WHERE companyRep=@companyRep", con);
+
+            //checks the database to see if user exists
+            con.Open();
+            rdr = cmd.ExecuteReader();
+            int dataCount = 0;
+
+            while (rdr.Read())
+            {
+                dataCount++;
+            }
+            con.Close();
+
+            //if user exists update the database from user input
+            if (dataCount > 0)
+            {
+                modify.Parameters.Add("@companyRep", System.Data.SqlDbType.VarChar).Value = member.CompanyRep;
+                modify.Parameters.Add("@phoneNumber", System.Data.SqlDbType.VarChar).Value = member.PhoneNumber;
+                modify.Parameters.Add("@address", System.Data.SqlDbType.VarChar).Value = member.Address;
+                modify.Parameters.Add("@suburb", System.Data.SqlDbType.VarChar).Value = member.Suburb;
+                modify.Parameters.Add("@postcode", System.Data.SqlDbType.VarChar).Value = member.Postcode;
+
+
+                con.Open();
+                modify.ExecuteNonQuery();
+                con.Close();
+                //lblProStatus.Text = "Details succesfully updated.";
+                lblModalTitle.Text = "CONGRATULATIONS!";
+                lblModalBody.Text = "Details succesfully updated.";
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "myModal", "$('#myModal').modal();", true);
+                upModal.Update();
+            }
+
+            //if user doesnt exist adds the user input to database
+            else
+            {
+                insert.Parameters.Add("@companyRep", System.Data.SqlDbType.VarChar).Value = member.CompanyRep;
+                insert.Parameters.Add("@phoneNumber", System.Data.SqlDbType.VarChar).Value = member.PhoneNumber;
+                insert.Parameters.Add("@address", System.Data.SqlDbType.VarChar).Value = member.Address;
+                insert.Parameters.Add("@suburb", System.Data.SqlDbType.VarChar).Value = member.Suburb;
+                insert.Parameters.Add("@postcode", System.Data.SqlDbType.VarChar).Value = member.Postcode;
+
+                insertId.Parameters.Add("@companyRep", System.Data.SqlDbType.VarChar).Value = member.CompanyRep;
+
+                con.Open();
+                insert.ExecuteNonQuery();
+                insertId.ExecuteNonQuery();
+                con.Close();
+
+                //lblProStatus.Text = "Details succesfully updated.";
+                lblModalTitle.Text = "CONGRATULATIONS!";
+                lblModalBody.Text = "Details succesfully updated.";
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "myModal", "$('#myModal').modal();", true);
+                upModal.Update();
+
+            }
         }
     }
 }
--- FollowMe302/FollowMe302/EditCompanyDetails.aspx.designer.cs
+++ FollowMe302/FollowMe302/EditCompanyDetails.aspx.designer.cs
@@ -85,6 +85,42 @@ namespace FollowMe302 {
         protected global::System.Web.UI.WebControls.Button btnEditCoDetails;
         
         /// <summary>
+        /// asm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager asm;
+        
+        /// <summary>
+        /// upModal control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel upModal;
+        
+        /// <summary>
+        /// lblModalTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblModalTitle;
+        
+        /// <summary>
+        /// lblModalBody control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblModalBody;
+        
+        /// <summary>
         /// lblEditCoStatus control.
         /// </summary>
         /// <remarks>
